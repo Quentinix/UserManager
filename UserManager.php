d5b5fca4f139f72e91afbdc09222b6704ed28d43
Quentinix <git@quentinix.fr>
2018-01-12 14:59:40 +0100

    Bug duplication class

--- UserManager.php
+++ UserManager.php
@@ -23,7 +23,6 @@ class UserManager extends Config
         // if (@file_exists(".configOK") == false and @file_exists("vendor/quentinix/usermanager/.configOK") == false) {
         //     throw new Exception("La configuration de UserManager n'est pas appliquée.");
         // } Solution reportée !
-        require 'Config.php';
         $this->sqlConnect = mysqli_connect($this->getConfigSqlHost(), $this->getConfigSqlUser(), $this->getConfigSqlPass(), $this->getConfigSqlDb());
         if (mysqli_errno($this->sqlConnect)) {
             throw new Exception("Echec requête SQL : " . mysqli_errno($this->sqlConnect) . " : " . mysqli_error($this->sqlConnect));
