2f751f6aa27809de2b35d0153e93c3d0fe90d55c
Quentinix <git@quentinix.fr>
2018-04-05 08:07:26 +0200

    Déplacement de la fonction createMdp()

--- class/Wave.php
+++ class/Wave.php
@@ -32,44 +32,4 @@ class Wave extends Config
         $composer = json_decode($composerFile);
         return $composer->version;
     }
-
-    /**
-     * Permet la création d'un mot de passe facile à retenir
-     *
-     * @return string
-     */
-    public function createMdp()
-    {
-        $lettreConsonne = array(
-            "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"
-        );
-        $lettreVoyelle = array(
-            "a", "e", "i", "o", "u", "y"
-        );
-        $lettreSpecial = array(
-            "&", "(", "-", "_", ")", "=", ",", ";", ":", "!", "$", "*"
-        );
-        $i = 0;
-        for ($i = 0; $i < 4; $i++) {
-            if ($i == 0) {
-                $rand = array_rand($lettreConsonne);
-                $return = strtoupper($lettreConsonne[$rand]);
-                $rand = array_rand($lettreVoyelle);
-                $return .= $lettreVoyelle[$rand];
-            }
-            if ($i == 1 || $i == 2) {
-                $rand = array_rand($lettreConsonne);
-                $return .= $lettreConsonne[$rand];
-                $rand = array_rand($lettreVoyelle);
-                $return .= $lettreVoyelle[$rand];
-            }
-            if ($i == 3) {
-                $rand = array_rand($lettreSpecial);
-                $return .= $lettreSpecial[$rand];
-                $return .= rand(0, 9);
-            }
-            $i++;
-        }
-        return $return;
-    }
 }
