fd6667d506d6b3eda3399e1abd1cec1a80a9ef68
Quentinix <git@quentinix.fr>
2018-04-11 17:31:37 +0200

    Modification des annotations

--- class/IpAccess.php
+++ class/IpAccess.php
@@ -6,7 +6,7 @@ namespace Wave;
 use Exception;
 
 /**
- * Classe de la gestion des permissions
+ * Classe de la gestion des permissions d'adresses IP
  *
  * @package  Wave
  * @author   Quentinix <git@quentinix.fr>
@@ -26,9 +26,14 @@ class IpAccess extends Config
     }
 
     /**
-     *
+     * Permet l'ajout d'une adresse IP autorisée sur un compte utilisateur
+     * Retourne true si l'adresse IP est bien ajoutée au compte utilisateur
+     * Retourne false si le compte utilisateur n'existe pas ou si l'adresse IP n'est pas correcte
+     * 
+     * @param String $user
+     * @param String $ip
      */
-    public function addIpAccess($user, $ip)
+    public function addIpAccess($user, $ip) // TODO: regex sur l'ip pour vérification
     {
         $sqlResult = mysqli_query($this->sqlConnect, "SELECT ip_access FROM " . $this->getConfigSqlTableUser . " WHERE `user` LIKE '" . $user . "' LIMIT 1");
         if (mysqli_errno($this->sqlConnect)) {
@@ -50,7 +55,12 @@ class IpAccess extends Config
     }
 
     /**
-     *
+     * Permet la suppression d'une adresse IP d'un compte utilisateur
+     * Retourne true si l'adresse IP est bien supprimée du compte utilisateur
+     * Retourne false si le compte utilisateur n'existe pas
+     * 
+     * @param String $user
+     * @param String $ip
      */
     public function removeIpAccess($user, $ip)
     {
